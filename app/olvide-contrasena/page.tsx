5fc3d107d91dced52a4f6e5ba0d616b8da3a881e
Bran258 <233349187+Bran258@users.noreply.github.com>
2026-03-22 21:37:40 -0500

    Prueba cookes

---
+++ app/olvide-contrasena/page.tsx
@@ -0,0 +1,7 @@
+export default function OlvideContrasenaPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1>Recuperar contraseña</h1>
+    </div>
+  );
+}
